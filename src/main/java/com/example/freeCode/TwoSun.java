47976fd82ffb5e620c42636372f9fe1c1919ebc4
DanielVegano <danielbtcethe22@gmail.com>
2026-09-20 00:06:16 -0300

    Target

--- src/main/java/com/example/freeCode/TwoSun.java
+++ src/main/java/com/example/freeCode/TwoSun.java
@@ -2,12 +2,28 @@ package com.example.freeCode;
 
 public class TwoSun {
 
+    public static void main(String []args){
+        leetCode leetcode = new leetCode();
+
+        leetcode.repost();
+    }
 
 }
 
 class leetCode{
 
-    public void repost(int x, int y){
+    public void repost(){
+
+        int[] nums = new int[4];
+        int target = 9;
+
+        /*saida [0,1]*/
+        /*Passando numeros*/
+        for (int i = 0; i < nums.length; i++){
+            System.out.println(nums[i]);
+            if()
+
+        }
 
     }
 
