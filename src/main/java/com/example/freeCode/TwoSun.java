1467483a4d8ddd6659105d6fd4831a8282b54428
DanielVegano <danielbtcethe22@gmail.com>
2026-09-20 00:43:41 -0300

    leet

--- src/main/java/com/example/freeCode/TwoSun.java
+++ src/main/java/com/example/freeCode/TwoSun.java
@@ -1,30 +1,40 @@
 package com.example.freeCode;
 
+
+
 public class TwoSun {
 
     public static void main(String []args){
-        leetCode leetcode = new leetCode();
 
-        leetcode.repost();
+    leetCode leetcode = new leetCode();
+
+    int[] nums = {4,1,5,2};
+    int target = 7;
+
+    int[] resultado = leetcode.twosun(nums,target);
+        System.out.println(resultado[0] + "," + resultado[1]);
+
+
     }
 
 }
 
 class leetCode{
 
-    public void repost(){
-
-        int[] nums = new int[4];
-        int target = 9;
+    public int[] twosun(int[] nums, int target) {
 
         /*saida [0,1]*/
         /*Passando numeros*/
-        for (int i = 0; i < nums.length; i++){
-            System.out.println(nums[i]);
-            if()
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
 
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
         }
-
+        return new int[]{};
+        }
     }
 
-}
+
