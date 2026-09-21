7469213a8b5a1eb14a167ce5476d8b6223c78410
DanielVegano <danielbtcethe22@gmail.com>
2026-09-21 10:52:44 -0300

    Lista

--- src/main/java/com/example/freeCode/FreeLista.java
+++ src/main/java/com/example/freeCode/FreeLista.java
@@ -1,46 +1,53 @@
 package com.example.freeCode;
 
 
+import com.example.todolist.todolistDois.model.Lista;
+
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class FreeLista {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
         Nomes nomes = new Nomes();
-        nomes.adicionar();
+        nomes.adicionar(scanner);
 
         /*NUMEROS INTEIROS*/
 
         Numeros numeros = new Numeros();
-        numeros.Inteiro();
+        numeros.Inteiro(scanner);
 
+        scanner.close();
     }
 }
 
 class Nomes{
-    public void adicionar (){
-
+    public void adicionar (Scanner scanner){
 
         ArrayList<String> ListaNomes = new ArrayList<>();
 
-        ListaNomes.add("Daniel");
-        ListaNomes.add("Fernando");
-        ListaNomes.add("Franzino");
+        System.out.print("Digite um nome de Produto: ");
+        String nome = scanner.nextLine();
+
+        ListaNomes.add(nome);
 
         System.out.println(ListaNomes);
     }
 }
 
 class Numeros{
-    public void Inteiro() {
+    public void Inteiro(Scanner scanner) {
 
         ArrayList<Integer> Inteiros = new ArrayList<>();
 
+        System.out.print("Digite a quantidade: ");
+        Integer numsINT = scanner.nextInt();
 
-        Inteiros.add(3);
-        Inteiros.add(1);
-        Inteiros.add(6);
+        Inteiros.add(numsINT);
+
 
         System.out.println(Inteiros);
     }
